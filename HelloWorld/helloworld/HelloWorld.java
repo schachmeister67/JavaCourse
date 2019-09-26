b75622d9e9021170d1395428b711834d4dc5a6ef
schachmeister67 <res1hlmv@gmail.com>
2019-09-26 17:56:28 -0500

    Add new file

---
+++ HelloWorld/helloworld/HelloWorld.java
@@ -0,0 +1,7 @@
+package helloworld; //group of related classes
+
+public class HelloWorld { // class name
+    public static void main(String[] args) { //entry point for all Java apps
+        System.out.println("Hello World"); //displays Hello World in the terminal
+    }
+}
