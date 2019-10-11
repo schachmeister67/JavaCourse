22078761a5ab89a9a51513e582907da4bfdb097d
schachmeister67 <res1hlmv@gmail.com>
2019-10-11 10:02:11 -0500

    Create Game.java

---
+++ MiniTennis/Game.java
@@ -0,0 +1,42 @@
+package minitennis;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Game extends JPanel {
+	
+	Ball ball = new Ball(this);
+	
+	private void move() {
+		ball.move();
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		ball.paint(g2d);
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		JFrame frame = new JFrame("Mini Tennis");
+		Game game = new Game();
+		frame.add(game);
+		frame.setSize(300,400);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		while (true) {
+			game.move();
+			game.repaint();
+			Thread.sleep(10);
+		}
+	}
+
+}
