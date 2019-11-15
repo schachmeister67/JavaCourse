a3d4a55720da1c841c309cd99a889ba8e5633441
schachmeister67 <res1hlmv@gmail.com>
2019-11-14 19:34:16 -0600

    Update MyButton.java

--- PuzzleGame/MyButton.java
+++ PuzzleGame/MyButton.java
@@ -1,1 +1,60 @@
+package puzzlegame;
 
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class MyButton extends JButton {
+
+	private static final long serialVersionUID = 5465381699813963236L;
+	
+	private boolean isLastButton;
+
+    public MyButton() {
+
+        super();
+
+        initUI();
+    }
+
+    public MyButton(Image image) {
+
+        super(new ImageIcon(image));
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        isLastButton = false;
+        BorderFactory.createLineBorder(Color.gray);
+
+        addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setBorder(BorderFactory.createLineBorder(Color.yellow));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setBorder(BorderFactory.createLineBorder(Color.gray));
+            }
+        });
+    }
+
+    public void setLastButton() {
+
+        isLastButton = true;
+    }
+
+    public boolean isLastButton() {
+
+        return isLastButton;
+    }
+}
